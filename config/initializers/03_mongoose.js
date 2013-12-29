d3a00ab5d53a61fa30680ece00c848e2c7d83895
Jean Friesewinkel <jeanfw@gmail.com>
2013-12-29 19:23:21 +0100

    adding mongoose types for emails and urls

--- config/initializers/03_mongoose.js
+++ config/initializers/03_mongoose.js
@@ -1,4 +1,6 @@
 module.exports = function() {
   this.mongoose = require('mongoose');
   this.mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/rubens");
+  var mongooseTypes = require("mongoose-types");
+  mongooseTypes.loadTypes(this.mongoose);
 }
